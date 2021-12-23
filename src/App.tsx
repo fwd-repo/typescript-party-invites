9cf13958ac1027560495196b83a49852c9bf96f5
Ed Tansey <office.fwd@yahoo.com>
2021-12-23 13:18:56 +0000

    Handle the state

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,18 @@ import React, { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
+interface IState {
+  people: {
+    name: string
+    age: number
+    url: string
+    note?: string
+  }[]
+}
+
 function App() {
+  const [people, setPeople] = useState<IState['people']>([])
+
   return (
     <div className='App'>
       <h1>People Invited to my Party</h1>
